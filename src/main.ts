9a4cea85ff3fe07c645c2afb2bf922481b87f064
Igli-Boci <igliboci10@gmai.com>
2022-07-20 09:34:03 +0200

    Adding basic state and header

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,32 @@
-import './style.css'
+import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+let state = {
+  landlord: [],
+  normalUser: [],
+  user: {
+    id: "",
+    password: "",
+    type: "",
+  },
+  properties: [
+    {
+      id: "",
+      location: "",
+      pricePerNight: "",
+      image: "",
+      availableDates: [],
+    },
+  ],
+};
+
+function renderHeader() {
+  let mainEl = document.querySelector("#app");
+  if (mainEl === null) return;
+
+  let headerEl = document.createElement("header");
+  headerEl.className = "header";
+  let h1El = document.createElement("h1");
+  h1El.textContent = "Looking.com";
+
+  mainEl.append(headerEl, h1El)
+}
